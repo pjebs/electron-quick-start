8b0ecfc86e01bf34f916cf82fed9743848db6e94
pj <pj@pjebs.com.au>
2020-02-15 23:35:25 +1100

    - Demonstrate issue: https://github.com/electron/electron/issues/22097

--- main.js
+++ main.js
@@ -2,40 +2,38 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
-function createWindow () {
-  // Create the browser window.
+// createWindow1 shows a non-kiosk window working as perfectly.
+// It demonstrates transparency + opacity setting.
+function createWindow1 () {
   const mainWindow = new BrowserWindow({
+    backgroundColor: '#FF0000',
     width: 800,
     height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    transparent: true,
+    opacity: 0.90, // 10% transparent
   })
-
-  // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 }
+
+// createWindow2 kiosk mode (+Fullscreen) window to show that it doesn't care about transparency.
+// Also demonstrate that opacity changes "darkness" instead of transparency.
+function createWindow2 () {
+  const mainWindow = new BrowserWindow({
+    backgroundColor: '#FFFFFF', // Adjust this
+    width: 800,
+    height: 600,
+    transparent: true,
+    opacity: 0.5, // 50% transparent <--- Adjust this
+    kiosk: true,
+  })
+  mainWindow.loadFile('index.html')
+}
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+app.on('ready', () => {
+  createWindow1()
+  setTimeout(() => { createWindow2() }, 2000);
 })
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
